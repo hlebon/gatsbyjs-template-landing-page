4daf8341370f5d8358aaabc39486ea4685366be0
hlebon <hanslgarcia@gmail.com>
2019-11-16 05:04:32 -0600

    basic styles and links

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
-import React from "react";
 import styled from "@emotion/styled";
 
 const HeaderContainer = styled.header`
@@ -8,30 +9,56 @@ const HeaderContainer = styled.header`
   top: 0;
   left: 0;
   right: 0;
-  min-height: 60px;
+  height: 60px;
   margin: 0 auto;
-  background: #fff;
+  background: transparent;
   z-index: 9;
 `;
 
 const HeaderContent = styled.div`
-  min-width: 800px;
+  max-width: 1200px;
+  height: inherit;
+  display: flex;
+  justify-content: space-between;
+  margin: auto;
 `;
+
+const styles = {
+  linkBtn: css`
+    padding: 5px;
+    margin: 5px;
+    align-self: center;
+  `,
+  linkHome: css`
+    color: #fff;
+    text-decoration: none;
+    line-height: 60px;
+  `,
+  linkList: css`
+    display: flex;
+    justify-content: space-between;
+  `,
+};
 
 const Header = ({ siteTitle }) => (
   <HeaderContainer>
     <HeaderContent>
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `gray`,
-            textDecoration: `none`,
-          }}
-        >
+      <h2 style={{ margin: 0 }}>
+        <Link to="/" css={styles.linkHome}>
           {siteTitle}
         </Link>
-      </h1>
+      </h2>
+      <div css={styles.linkList}>
+        <a href="#id1" css={styles.linkBtn}>
+          section 1
+        </a>
+        <a href="#id2" css={styles.linkBtn}>
+          section 2
+        </a>
+        <a href="#id3" css={styles.linkBtn}>
+          section 3
+        </a>
+      </div>
     </HeaderContent>
   </HeaderContainer>
 );
